1fb80925234a12ec5b54c8e5ab06d55484f4498b
dely <lhyal2p@gmail.com>
2024-10-14 15:31:59 +0900

    프로젝트 생성

---
+++ MyCleanProject/MyCleanProjectTests/MyCleanProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyCleanProjectTests.swift
+//  MyCleanProjectTests
+//
+//  Created by elly on 10/14/24.
+//
+
+import Testing
+@testable import MyCleanProject
+
+struct MyCleanProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
