19aa0f01d89884a724b713c529be88f6f402d375
dely <lhyal2p@gmail.com>
2024-10-22 22:41:29 +0900

    Usecase 구현

---
+++ MyCleanProject/MyCleanProject/Domain/Usecase/UserListUsecase.swift
@@ -0,0 +1,51 @@
+//
+//  UserListUsecase.swift
+//  MyCleanProject
+//
+//  Created by elly on 10/22/24.
+//
+
+import Foundation
+
+public protocol UserListUsecaseProtocol {
+    /// 유저 리스트 불러오기 (원격)
+    func fetchUser(query: String, page: Int) async -> Result<UserListResult, NetworkError>
+    
+    /// 전체 즐겨찾기 리스트 불러오기
+    func getFavoriteUsers() -> Result<[UserListItem], CoreDataError>
+    
+    /// 즐겨찾기 유저 저장
+    func saveFavoriteUser(user: UserListItem) -> Result<Bool, CoreDataError>
+    
+    /// 즐겨찾기 유저 삭제
+    func deleteFavoriteUser(userID: Int) -> Result<Bool, CoreDataError>
+    
+    
+    /// 배열 -> Dicttionary [초성: [유저리스트]]
+    
+    /// 유저리스트 - 즐겨찾기 포함된 유저인지
+}
+
+public struct UserListUsercase: UserListUsecaseProtocol {
+    private let repository: UserRepositoryProtocol
+    
+    init(repository: UserRepositoryProtocol) {
+        self.repository = repository
+    }
+    
+    public func fetchUser(query: String, page: Int) async -> Result<UserListResult, NetworkError> {
+        await repository.fetchUser(query: query, page: page)
+    }
+    
+    public func getFavoriteUsers() -> Result<[UserListItem], CoreDataError> {
+        repository.getFavoriteUsers()
+    }
+    
+    public func saveFavoriteUser(user: UserListItem) -> Result<Bool, CoreDataError> {
+        repository.saveFavoriteUser(user: user)
+    }
+    
+    public func deleteFavoriteUser(userID: Int) -> Result<Bool, CoreDataError> {
+        repository.deleteFavoriteUser(userID: userID)
+    }
+}
